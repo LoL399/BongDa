0e2d6d3b3269d7c0f3106e08bb345bf3de9e980c
LoL3999 <tvloi3999@gmail.com>
2020-08-24 11:38:38 +0700

    Done detail, 80% index

--- SoccerWorld/SoccerWorld/Areas/User/Controllers/HomeController.cs
+++ SoccerWorld/SoccerWorld/Areas/User/Controllers/HomeController.cs
@@ -15,18 +15,67 @@ namespace SoccerWorld.Areas.User.Controllers
         // GET: User/Home
         public ActionResult Index()
         {
-            return View();
+            List<PhanLoai> phanloai = dbm.PhanLoais.ToList();
+            List<PhanLoai> phanloai1 = new List<PhanLoai>();
+            foreach (var x in phanloai)
+            {
+                if (checkAvibility(x.IDPhanLoai))
+                    phanloai1.Add(x);
+                
+            }
+            return View(phanloai1);
         }
-        public ActionResult Product()
+        public ActionResult GetCardInfo(int id)
         {
-            int id = 1;
+            List<SanPham> sp = dbm.SanPhams.Where(x => x.IDPhanLoai == id).Take(5).ToList();
+            return PartialView(sp);
+        }
+        public ActionResult Category()
+        {
+            List<PhanLoai> phanLoai = dbm.PhanLoais.ToList();
+            return PartialView("_Category", phanLoai);
+        }
+
+        public ActionResult Product(int id)
+        {
             SanPham sp = new SanPham();
             sp = dbm.SanPhams.Where(x => x.ID.ToString() == id.ToString()).FirstOrDefault();
             var phanloai = dbm.PhanLoais.FirstOrDefault(x => x.IDPhanLoai == sp.IDPhanLoai);
-            List<CTKichCo> kichco = dbm.CTKichCoes.Where(x => x.IDSP == sp.ID && x.TinhTrang==true).ToList();
+            
+            
             ViewBag.PhanLoai = phanloai.TenPhanLoai.ToString();
-            ViewBag.KichCo = kichco;
             return View(sp);
+        }
+        public ActionResult GetSize(int id)
+        {
+            List<CTKichCo> lstkichco = dbm.CTKichCoes.Where(x => x.IDSP == id && x.TinhTrang == true).ToList();
+            List<Kichco> kichco = new List<Kichco>();
+            foreach (var x in lstkichco)
+            {
+                kichco.Add(dbm.Kichcoes.Where(o => o.IDKichCo == x.IDKC).FirstOrDefault());
+            }
+            return PartialView(kichco);
+        }
+        public ActionResult GetImg(int id)
+        {
+            List<HinhAnh> hinhanh = dbm.HinhAnhs.Where(x => x.IDSP == id).ToList();
+            return PartialView(hinhanh);
+        }
+
+        public ActionResult GetMau(int id)
+        {
+            List<ChiTietMau> lstmau = dbm.ChiTietMaus.Where(x => x.IDSP == id).ToList();
+            List<MauSac> mau = new List<MauSac>();
+            foreach (var x in lstmau)
+            {
+                mau.Add(dbm.MauSacs.Where(o => o.IDMau == x.IDMau).FirstOrDefault());
+            }
+            return PartialView(mau);
+        }
+        public bool checkAvibility(int id)
+        {
+            return dbm.SanPhams.Any(x => x.IDPhanLoai == id);
+            
         }
     }
 }
